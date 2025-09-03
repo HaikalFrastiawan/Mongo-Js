1bbaa809b1774df05f109d711e7bb5644e2399c1
HaikalFrastiawan <haikalfrastiawan16@gmail.com>
2025-09-03 19:26:48 +0700

    insert Many

--- index.js
+++ index.js
@@ -7,20 +7,55 @@ mongoose.connect('mongodb://127.0.0.1:27017/movie-db')
 
 const movieSchema = new mongoose.Schema({
     title: String,
+    genre: String,
     year: Number,
-    score: Number,
+    rating: Number,
     director: String
 })
 
 const Movie = mongoose.model('Movie', movieSchema)
 
-const movie = new Movie ({
+// const movie = new Movie ({
+//     title: 'Spiderman',
+//     genre: 'action',
+//     year: 2025,
+//     rating: 10,
+//     director: 'Repan'
+// })
+
+// Insert 1 data
+const movie = new Movie({
     title: 'Spiderman',
     year: 2025,
-    score: 10,
-    director: 'Repan'
-})
+    rating: 10,
+    director: 'Repan',
+    genre: 'Action'
+});
 
-movie.save() //untuk menyimpan ke mongo-db
+movie.save()
+    .then(res => {
+        console.log("One movie inserted:", res);
+    })
+    .catch(err => console.log("Error:", err));
+
+
+// Insert banyak data
+const movies = [
+    { title: 'Batman', year: 2024, rating: 9, director: 'Nolan', genre: 'Action' },
+    { title: 'Iron Man', year: 2008, rating: 8.5, director: 'Jon Favreau', genre: 'Sci-Fi' },
+    { title: 'Avengers', year: 2012, rating: 9.2, director: 'Joss Whedon', genre: 'Action' },
+    { title: 'Doctor Strange', year: 2016, rating: 8.7, director: 'Scott Derrickson', genre: 'Fantasy' }
+];
+
+Movie.insertMany(movies)
+    .then(res => {
+        console.log("it's Work:", res);
+    })
+    .catch(err => {
+        console.log("Error inserting movies:", err);
+    });
+
+
+
 
 console.log(movie)
